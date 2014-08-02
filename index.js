36a9c68b5bb49f0a67789e0d7f79d60ba1000d82
Sindre Sorhus <sindresorhus@gmail.com>
2014-08-02 21:12:57 +0200

    minor tweaks

--- index.js
+++ index.js
@@ -21,13 +21,16 @@ module.exports = function (options) {
 
 		try {
 			var fileOptions = objectAssign({}, options);
+
 			if (file.sourceMap) {
-				fileOptions.sourceFileName = fileOptions.sourceFileName || file.relative;
-				fileOptions.sourceMapName = fileOptions.sourceMapName || file.relative;
+				fileOptions.sourceFileName = file.relative;
+				fileOptions.sourceMapName = file.relative;
 			}
+
 			var res = esnext.compile(file.contents.toString(), fileOptions);
 
 			file.contents = new Buffer(res.code);
+
 			if (res.map && file.sourceMap) {
 				applySourceMap(file, res.map);
 			}
